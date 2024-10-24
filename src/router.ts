75cc0a5687f4e9541a0798984cc0d38060084a20
Kayky Dion <kayky.dion@gmail.com>
2024-10-24 16:33:04 -0300

    feat: create leads table manipulation routes

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { HttpError } from "./errors/HttpError";
+import { LeadsController } from "./controllers/LeadsController";
 
 const router = Router();
+
+const leadsController = new LeadsController();
+
+router.get("/leads", leadsController.index);
+router.post("/leads", leadsController.create);
+router.get("/leads/:id", leadsController.show);
+router.delete("/leads/:id", leadsController.delete);
+router.put("/leads/:id", leadsController.update);
 
 router.get("/status", (req, res, next) => {
   try {
